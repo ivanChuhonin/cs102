32c876be9183bc77e8bbd3f23dfff535bca09053
ivanChuhonin <90215201+ivanChuhonin@users.noreply.github.com>
2022-03-16 02:35:31 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,7 +1,7 @@
 import string
 
 dictA = [chr(i) for i in range(65, 91)]
-dict = list(string.ascii_uppercase)# + dicta
+dict = list(string.ascii_uppercase) # + dicta
 
 def findIdx(c):
     return dict.index(c)
@@ -25,7 +25,7 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     lk = len(Ukeyword)
     repeatN = l // lk
     remainder = l % lk
-    fullKey = Ukeyword * repeatN + Ukeyword[: remainder]
+    fullKey = Ukeyword * repeatN + Ukeyword[:remainder]
     result = []
     for idx, val in enumerate(Uplaintext):
         if plaintext[idx] in string.ascii_letters:
@@ -36,7 +36,7 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
                 result.append(dict[(key % base)].lower())
         else:
             result.append(plaintext[idx])
-    return ''.join(result)
+    return "".join(result)
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -57,7 +57,7 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     lk = len(keyword)
     repeatN = l // lk
     remainder = l % lk
-    fullKey = Ukeyword * repeatN + Ukeyword[: remainder]
+    fullKey = Ukeyword * repeatN + Ukeyword[:remainder]
     result = []
     for idx, val in enumerate(Uciphertext):
         if ciphertext[idx] in string.ascii_letters:
@@ -68,10 +68,10 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
                 result.append(dict[(key % base)].lower())
         else:
             result.append(ciphertext[idx])
-    return ''.join(result)
+    return "".join(result)
 
 
-if __name__ == '__main__':
-    y = encrypt_vigenere('ATTACKATDAWN', 'LEMON')
+if __name__ == "__main__":
+    y = encrypt_vigenere("ATTACKATDAWN", "LEMON")
     print(y)
-    print(decrypt_vigenere(y, 'LEMON'))
+    print(decrypt_vigenere(y, "LEMON"))
